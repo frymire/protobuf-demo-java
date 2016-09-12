a15597aa4025155fc9b05ebc222c30e1f48c31bd
Frymire, Mark E <mark.frymire@dac.us>
2016-09-12 12:28:32 -0400

    Add initial demo of ProtoBuf capability

---
+++ src/com/example/tutorial/Hi.java
@@ -0,0 +1,22 @@
+package com.example.tutorial;
+// UNCLASSIFIED
+
+/**
+ * class_comment_here
+ *
+ * <p>(C) Copyright DECISIVE ANALYTICS Corporation 2016<br>
+ * All Rights Reserved<br>
+ * DAC Proprietary<br>
+ *
+ */
+public class Hi {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hi.");
+
+  }
+
+}
