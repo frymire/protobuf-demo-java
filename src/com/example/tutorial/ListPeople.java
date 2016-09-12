b9ddfb921a1481d30ef1b570903e1230df8717e2
Frymire, Mark E. <mark.frymire@dac.us>
2016-09-12 13:27:18 -0400

    Simplify demo code

--- src/com/example/tutorial/ListPeople.java
+++ src/com/example/tutorial/ListPeople.java
@@ -4,19 +4,19 @@ import com.example.tutorial.AddressBookProtos.AddressBook;
 import com.example.tutorial.AddressBookProtos.Person;
 
 import java.io.FileInputStream;
+import java.util.List;
 
 class ListPeople {
 
-  static void Print(AddressBook addressBook) {
+  public static void main(String[] args) throws Exception {
+
+    List<Person> people = AddressBook.parseFrom(new FileInputStream("MyAddressBook")).getPeopleList();
     
-    // Iterate though all people in the AddressBook and print info about them.
-    for (Person person: addressBook.getPeopleList()) {
-      
+    for (Person person: people) {
+
       System.out.println("Person ID: " + person.getId());
       System.out.println("  Name: " + person.getName());
-      if (!person.getEmail().isEmpty()) {
-        System.out.println("  E-mail address: " + person.getEmail());
-      }
+      if (!person.getEmail().isEmpty()) System.out.println("  E-mail address: " + person.getEmail());
 
       for (Person.PhoneNumber phoneNumber : person.getPhonesList()) {
         switch (phoneNumber.getType()) {
@@ -29,23 +29,13 @@ class ListPeople {
         case WORK:
           System.out.print("  Work phone #: ");
           break;
+        case UNRECOGNIZED:
+          break;
         }
+        
         System.out.println(phoneNumber.getNumber());
       }
-    }
-  }
 
-  // Main function:  Reads the entire address book from a file and prints all
-  //   the information inside.
-  public static void main(String[] args) throws Exception {
-    if (args.length != 1) {
-      System.err.println("Usage:  ListPeople ADDRESS_BOOK_FILE");
-      System.exit(-1);
-    }
-
-    // Read the existing address book.
-    AddressBook addressBook = AddressBook.parseFrom(new FileInputStream(args[0]));
-
-    Print(addressBook);
+    }    
   }
 }
